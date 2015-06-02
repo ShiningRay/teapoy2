3739bdbcbc2be5d450f5195f4ae484b3c5534cad
ShiningRay <tsowly@hotmail.com>
2015-06-03 01:08:48 +0800

    fix flash uploader

--- app/posts/flash.rb
+++ app/posts/flash.rb
@@ -1,10 +1,11 @@
 # coding: utf-8
 class Flash < Post
-  has_mongoid_attached_file :swf
+  mount_uploader :swf, FlashUploader
+  # has_mongoid_attached_file :swf
 
-  validates_attachment_content_type :swf,
-    :content_type => ['application/x-shockwave-flash' ]
-  validates_attachment_size :swf, :less_than => 15.megabytes
+  # validates_attachment_content_type :swf,
+  #   :content_type => ['application/x-shockwave-flash' ]
+  # validates_attachment_size :swf, :less_than => 15.megabytes
 
   def to_s
     swf.url(:original)
