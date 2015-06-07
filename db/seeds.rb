5ad2d791a1e12327d26c171d0705b521654d5eed
Zhang Wen <zhangwen1932@sina.com>
2015-06-08 01:52:38 +0800

    add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create login: 'guest', name: 'guest', password: '1234qwer!@#$', password_confirmation: '1234qwer!@#$', email: 'guest@bling0.com'
+User.connection.execute "update users set id = 0 where login like 'guest'"
+r = Role.find_or_create_by name: 'admin'
+u = User.create login: 'shiningray', name: 'ShiningRay', password: '1234qwer', password_confirmation: '1234qwer', email: 'shiningray@bling0.com'
+u.roles << r
+u.status='active'
+u.save
+Group.create :alias => 'pool', name: '水库', owner_id: u.id
