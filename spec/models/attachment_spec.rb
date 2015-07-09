28e8a3106d14cb2d448125b2f29254392cb4ce79
ShiningRay <tsowly@hotmail.com>
2015-07-10 01:39:56 +0800

    save image dimensions spec

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,6 +1,24 @@
 # coding: utf-8
+
+
 require 'rails_helper'
 
 describe Attachment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:file) { File.open Rails.root.join('spec/fixtures/2345.jpg'), 'rb'}
+
+  describe '.dimensions' do
+    before do
+      CarrierWave.configure do |config|
+        config.enable_processing = true
+      end
+    end
+
+    after do
+      CarrierWave.configure do |config|
+        config.enable_processing = false
+      end
+    end
+    subject { Attachment.create file: file}
+    its(:dimensions) { should_not be_blank }
+  end
 end
