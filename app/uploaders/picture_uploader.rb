65fb2d0f0390e0b220f2f817764e0b9671a9c4b3
ShiningRay <tsowly@hotmail.com>
2015-07-10 22:48:52 +0800

    fix upload path

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -34,6 +34,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def store_dir
   #   # "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
   #
   # process :store_dimensions
 
