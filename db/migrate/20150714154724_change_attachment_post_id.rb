311d9f81190e961222f33e2df316bb9f610e2b60
ShiningRay <tsowly@hotmail.com>
2015-07-15 18:19:01 +0800

    fix

--- db/migrate/20150714154724_change_attachment_post_id.rb
+++ db/migrate/20150714154724_change_attachment_post_id.rb
@@ -1,5 +1,5 @@
 class ChangeAttachmentPostId < ActiveRecord::Migration
   def change
-    change_column :attachments, :post_id, :integer, null: false
+    change_column :attachments, :post_id, :integer
   end
 end
